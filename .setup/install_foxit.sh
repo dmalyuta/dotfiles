b2d36f0cbb4c0469915e4ffbb693ee054f63e325
Danylo Malyuta <danylo.malyuta@gmail.com>
2016-11-19 22:41:23 +0100

    foxit install script fix

--- .setup/install_foxit.sh
+++ .setup/install_foxit.sh
@@ -28,7 +28,8 @@ if program_not_installed "FoxitReader"; then
     foxit_installer_name=$(ls FoxitReader*.run)
 	
     # install Foxit PDF Reader
-    echowarn "Please follow the GUI's instructions and install Foxit PDF Reader into /opt/foxitsoftware/foxitreader"  
+    echowarn "Please follow the GUI's instructions and install Foxit PDF Reader into /opt/foxitsoftware/foxitreader"
+    runcmd "pwd"
     runcmd "sudo ./'$foxit_installer_name'"
     installer_exit_status=$?
 

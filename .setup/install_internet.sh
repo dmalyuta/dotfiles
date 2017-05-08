7a3d7147fc3d353bdd4a9346abe4f381259a9745
dmalyuta <danylo.malyuta@gmail.com>
2017-05-08 02:18:07 -0700

    added Insync Google Drive client installation

--- .setup/install_internet.sh
+++ .setup/install_internet.sh
@@ -19,4 +19,12 @@ if program_not_installed "google-chrome"; then
     runcmd "rm -f ${home}/Downloads/chrome.deb"
 fi
 
+# Insync Google Drive client
+if program_not_installed "insync"; then
+	runcmd "wget https://d2t3ff60b2tol4.cloudfront.net/builds/insync_1.3.16.36155-trusty_amd64.deb -O ${home}/Downloads/insync.deb"
+    runcmd_noexit "dpkg -i ${home}/Downloads/insync.deb" nonull
+    runcmd "apt-get --assume-yes install -f" nonull
+    runcmd "rm -f ${home}/Downloads/insync.deb"
+fi
+
 echo_prefix="$echo_prefix_temp"

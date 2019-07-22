0e78232fa1aa200b51eb5ebc5ae588e58738674d
Danylo Malyuta <danylo.malyuta@gmail.com>
2019-07-22 08:16:51 -0700

    add Mailspring email client

--- .setup/install_internet.sh
+++ .setup/install_internet.sh
@@ -20,9 +20,18 @@ fi
 
 # Dropbox
 if program_not_installed "dropbox"; then
-	runcmd "wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb -O /tmp/dropbox.deb"
+    runcmd "wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb -O /tmp/dropbox.deb"
     runcmd_noexit "dpkg -i /tmp/dropbox.deb" nonull
     runcmd "apt-get --assume-yes install -f" nonull
+fi
+
+# Mailspring
+if program_not_installed "mailspring"l then
+   apt_get_install_pkg libsecret-1-dev
+   apt_get_install_pkg gir1.2-gnomekeyring-1.0
+   runcmd "wget https://updates.getmailspring.com/download?platform=linuxDeb -O /tmp/mailspring.deb"
+   runcmd_noexit "dpkg -i /tmp/mailspring.deb" nonull
+   runcmd "apt-get --assume-yes install -f" nonull
 fi
 
 echo_prefix="$echo_prefix_temp"

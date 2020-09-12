a0628d20d68afdcb83c2e0fd12f078b3e371aa0d
Danylo Malyuta <danylo@malyuta.name>
2020-09-12 13:52:55 -0700

    Minor change

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -335,6 +335,7 @@
   ;; C, C++, and Objective-C language.
   :ensure t
   :config
+  (require 'ccls)
   (setq ccls-executable "ccls") ;; Must be on PATH: assume /usr/local/bin/ccls
   (setq lsp-prefer-flymake nil)
   (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))

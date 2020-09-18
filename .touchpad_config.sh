84ccb608479ff6e8adb400fb13783fb3e77013fb
Danylo Malyuta <danylo@malyuta.name>
2020-09-18 13:33:25 -0700

    Updates to touchpad setup. COnverged to using Synpatics for both Logitech and Seenda

--- .touchpad_config.sh
+++ .touchpad_config.sh
@@ -6,9 +6,11 @@
 # ----------------------------------------------------------------------
 
 #####################################
-# ..:: Synaptics touchpad (designed for Logitech T650 external touchpad) ::..
+# ..:: Synaptics touchpad ::..
 
 ERRF=/tmp/touchpad_errors
+
+# ..:: Logitech T650 external touchpad ::..
 
 xinput set-prop "Logitech Rechargeable Touchpad T650" "Synaptics Edges" 113 2719 127 2237 &>>$ERRF
 xinput set-prop "Logitech Rechargeable Touchpad T650" "Synaptics Finger" 1 1 0 &>>$ERRF
@@ -24,13 +26,17 @@ xinput set-prop "Logitech Rechargeable Touchpad T650" "Synaptics Noise Cancellat
 xinput set-prop "Logitech Rechargeable Touchpad T650" "Synaptics Circular Scrolling" 0 &>>$ERRF
 xinput set-prop "Logitech Rechargeable Touchpad T650" "Synaptics Coasting Speed" 0 0 &>>$ERRF
 xinput set-prop "Logitech Rechargeable Touchpad T650" "Synaptics Circular Pad" 0 &>>$ERRF
+# Quadratic acceleration profile -- feels better
+xinput set-prop "Logitech Rechargeable Touchpad T650" "Device Accel Profile" 2 &>>$ERRF
 
-xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Finger" 3 5 0 &>>$ERRF
-xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Tap Time" 90 &>>$ERRF
+# ..:: Seenda external touchpad ::..
+
+xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Finger" 1 1 0 &>>$ERRF
+xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Tap Time" 120 &>>$ERRF
 xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Tap Move" 30 &>>$ERRF
 # SingleTapTimeout, MaxDoubleTapTime, ClickTime
 xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Tap Durations" 180 180 100 &>>$ERRF
-xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Move Speed" 1.8 1.8 0 0 &>>$ERRF
+xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Move Speed" 2.2 2.2 0 0 &>>$ERRF
 xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Tap Action" 0 0 0 0 1 3 2 &>>$ERRF
 xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Click Action" 0 0 0 &>>$ERRF
 xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Palm Detection" 0 &>>$ERRF
@@ -39,3 +45,5 @@ xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Noise Ca
 xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Circular Scrolling" 0 &>>$ERRF
 xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Coasting Speed" 0 0 &>>$ERRF
 xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Synaptics Circular Pad" 0 &>>$ERRF
+# Quadratic acceleration profile -- feels better
+xinput set-prop "HTX USB HID Device HTX HID Device Touchpad" "Device Accel Profile" 2 &>>$ERRF

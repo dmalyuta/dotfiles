19cfd9e2f107ec68a4759c64ac6bb81b22a2ca02
Danylo Malyuta <12475811+dmalyuta@users.noreply.github.com>
2022-01-09 00:18:11 -0800

    Tmux improvements, VS code quicksuggest increase delay

---
+++ .bin/tmux-window-renumber.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Re-numbers windows so that they all again start from zero and go up in
+# contiguous numbers
+CURRENT_WINDOW=$(tmux display-message -p '#W')
+LAST_WINDOW=-1
+tmux list-windows | while read line; do
+    WINDOW_NUMBER=$(echo $line | sed 's/^\([0-9]\+\):.*/\1/')
+    if (( $WINDOW_NUMBER > $LAST_WINDOW+1 )); then
+        tmux move-window -s $WINDOW_NUMBER -t $(( $LAST_WINDOW+1 ))
+    fi
+    LAST_WINDOW=$(( $LAST_WINDOW+1 ))
+done
+tmux select-window -t $CURRENT_WINDOW

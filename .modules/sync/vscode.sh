6f6c0b86e00f59d52863c86f4a096113ac3d08e6
Danylo Malyuta <12475811+dmalyuta@users.noreply.github.com>
2022-02-09 00:23:19 -0800

    Sync VS Code settings in clean state

---
+++ .modules/sync/vscode.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Syncing script for VS Code, to maintain consistency when pushing to remote
+# repo.
+#
+# Author: Danylo Malyuta, 2022.
+
+vscode_cleanup_settings() {
+    SETTINGS_FILE_PATH=$DIR/.vscode/settings.json
+    TAB=$(printf '\%.0s ' {1..4})
+
+    # Save the current version
+    cp $SETTINGS_FILE_PATH /tmp/settings.json.bk
+
+    # Operate on settings.json
+    # - Make sure that activity bar is hidden
+    sed -i '/workbench\.activityBar\.visible/d' $SETTINGS_FILE_PATH
+    sed -i '/\/\/\/ Appearance/a'"$TAB"'"workbench.activityBar.visible": true,' \
+        $SETTINGS_FILE_PATH
+}
+
+vscode_restore_settings() {
+    SETTINGS_FILE_PATH=$DIR/.vscode/settings.json
+    mv /tmp/settings.json.bk $SETTINGS_FILE_PATH
+}
